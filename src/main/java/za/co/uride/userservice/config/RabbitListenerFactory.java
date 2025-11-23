18e7ff32c6deb5d9da1b14b2c4af9285942b8583
mpendulom <mpendulomalangwane@yahoo.com>
2025-11-23 11:47:47 +0200

    registration flow change and queue consumation messages

--- src/main/java/za/co/uride/userservice/config/RabbitListenerFactory.java
+++ src/main/java/za/co/uride/userservice/config/RabbitListenerFactory.java
@@ -1,23 +1,26 @@
 package za.co.uride.userservice.config;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitListenerFactory {
     @Bean
-    public SimpleRabbitListenerContainerFactory userVirtualHostListenerContainerFactory(UserVirtualHost userVirtualHost) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    public SimpleMessageListenerContainer userVirtualHostListenerContainerFactory(UserVirtualHost userVirtualHost) {
+        SimpleMessageListenerContainer factory = new SimpleMessageListenerContainer();
         factory.setConnectionFactory(userVirtualHost.connectionFactory());
+        factory.setAutoStartup(true);
         return factory;
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory notificationVirtualHostListenerContainerFactory(
+    public SimpleMessageListenerContainer notificationVirtualHostListenerContainerFactory(
             NotificationVirtualHost notificationVirtualHost) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        SimpleMessageListenerContainer factory = new SimpleMessageListenerContainer();
         factory.setConnectionFactory(notificationVirtualHost.connectionFactory());
+        factory.setAutoStartup(true);
         return factory;
     }
 }
