2f9ef0cd922fefbc4c01cfa1945bb2742fbc30f3
mpendulom <mpendulomalangwane@yahoo.com>
2025-12-18 14:01:55 +0200

    adding find all route for driver address search

--- src/main/resources/db/migration/V55__add_new_entitlement.sql
+++ src/main/resources/db/migration/V55__add_new_entitlement.sql
@@ -1,5 +1,5 @@
 
-@roleId= (select id from role where role_name='driver' and system_name='uride');
+set @roleId= (select id from role where role_name='driver' and system_name='uride');
 INSERT ignore INTO user.entitlement (name, last_modified_on, status)
 VALUES ('find-all-route', sysdate(), true);
 INSERT ignore INTO user.role_entitlement (role_id, entitlement_id, last_modified_on, status)
