2ee2b939745603bb396db797ffddcbaf3e90433c
mpendulom <mpendulomalangwane@yahoo.com>
2025-12-28 18:51:42 +0200

    adding new entitlements for messages

---
+++ src/main/resources/db/migration/V58__add_new_entitlement.sql
@@ -0,0 +1,26 @@
+set
+@driverRoleId= (select id from role where role_name='driver' and system_name='uride');
+set
+@passengerRoleId= (select id from role where role_name='passenger' and system_name='uride');
+INSERT
+ignore INTO user.entitlement (name, last_modified_on, status)
+VALUES ('find-trip-request-rating', sysdate(), true);
+set
+@entitlmentId =  LAST_INSERT_ID();
+INSERT
+ignore INTO user.role_entitlement (role_id, entitlement_id, last_modified_on, status)
+VALUES (@driverRoleId, @entitlmentId, sysdate(), true);
+
+INSERT
+ignore INTO user.role_entitlement (role_id, entitlement_id, last_modified_on, status)
+VALUES (@passengerRoleId, @entitlmentId, sysdate(), true);
+
+INSERT
+ignore INTO user.entitlement (name, last_modified_on, status)
+VALUES ('edit-trip-request-rating', sysdate(), true);
+set
+@entitlmentId =   LAST_INSERT_ID();
+
+INSERT
+ignore INTO user.role_entitlement (role_id, entitlement_id, last_modified_on, status)
+VALUES (@passengerRoleId, @entitlmentId, sysdate(), true);
